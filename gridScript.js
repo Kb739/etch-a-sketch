9f31736aeda7936e034359d7ea12be45d538800f
kunal <kunalbankoti9@gmail.com>
2022-05-17 12:44:05 +0530

    add event to button to create grid with dynamic size

--- gridScript.js
+++ gridScript.js
@@ -1,10 +1,38 @@
-const grid = document.querySelector('#grid-container');
-const count = 10;
-let root = document.querySelector(':root');
-root.style = `--width:${100 / count}%;--height:${100 / count}%`;
 
-for (let i = 0; i < count * count; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    grid.appendChild(square);
+
+const grid = document.querySelector('#grid-container');
+const root = document.querySelector(':root');
+const button = document.querySelector('button');
+
+button.addEventListener('click', inputGridSize);
+
+function inputGridSize(e) {
+    let input = prompt('input grid size');
+    if (input) {
+        input = Number(input);
+        if (input) {
+            deleteOldGrid();
+            createGrid(input);
+        }
+        else {
+            alert('invalid input : enter a number');
+        }
+    }
+}
+
+function createGrid(gridSize) {
+    root.style = `--width:${100.0 / gridSize}%;--height:${100.0 / gridSize}%`;
+    const blockCount = gridSize * gridSize;
+    for (let i = 0; i < blockCount; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        grid.appendChild(square);
+    }
+}
+function deleteOldGrid() {
+    if (grid.hasChildNodes()) {
+        const blocks=document.querySelectorAll('#grid-container>div');
+        blocks.forEach(node=>node.remove());
+    }
+    
 }
